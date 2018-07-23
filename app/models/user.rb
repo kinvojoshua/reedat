3a7094006f5f0a4919529538f6456d1d898b03b6
JedBautista <bautista.jpeb@gmail.com>
2018-07-23 17:45:48 +0800

    Fixed navbar for user added dropdown

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
-class User < ApplicationRecord
 
+class User < ApplicationRecord
+ 
   has_secure_password
-  has_many :posts
+  has_many :posts   
   has_many :comments
   has_many :votes
 
-  validates :first_name, presence:  true
+  validates :first_name, presence: true
   validates :last_name, presence: true
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@.\s]+\.)*[^@.\s]+\z/ }
