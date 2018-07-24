cc2d1fb6c23d4cf85d20d0dc5e1962e8048066fe
Joshua <niputen13@gmail.com>
2018-07-24 13:02:58 +0800

    fixed devise bugs

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -9,9 +9,15 @@ class LandingController < ApplicationController
   def authenticate
     @user = User.find_for_authentication(username: params[:username])
     @user.valid_password?(params[:password]) ? @user: nil
-    unless @user.nil?
+    if @user.present?
       sign_in(@user)
       redirect_to root_path
+    else
+      redirect_to login_path
     end
+  end
+
+  def logout
+
   end
 end
